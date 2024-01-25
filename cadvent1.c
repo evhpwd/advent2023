09ff0d7a15615fcb7019dc8794c0613c4079b3c7
evhpwd <147830295+evhpwd@users.noreply.github.com>
2024-01-25 13:23:25 +0000

    1st challenge solution in C

---
+++ cadvent1.c
@@ -0,0 +1,30 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+FILE* fptr;
+
+int main() {
+	fptr = fopen("input1.txt", "r");
+	char* line = malloc(100);
+
+	int total = 0;
+	while (fgets(line, 100, fptr)) {
+		int firstNum = 0;
+		int lastNum = 0;
+		for (int i = 0; i < strlen(line); i++) {
+			//printf("%c\n", token[i]);
+			char curChar = line[i];
+			if (curChar >= '0' && curChar <= '9') {
+				if (firstNum == 0) {
+					firstNum = curChar - '0';
+				}
+				lastNum = curChar - '0';
+			}
+		}
+		total += (10 * firstNum) + lastNum;
+	}
+	
+	printf("\n%d", total);
+	fclose(fptr);
+}
